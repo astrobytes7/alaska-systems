69a821510db462738bde7b15b06894c6f74ed21f
Eshan <astrobytesgaming@gmail.com>
2026-05-02 12:18:55 -0700

    alaska update :)

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
     TOKEN: process.env.TOKEN,
     APP_ID: process.env.APP_ID,
     MONGOURL: "mongodb+srv://notastrobytes_db_user:king@alaska.lyqfdw2.mongodb.net/",
+    DOCK_API: "hCD0GAZoJD8v.96d2Es.YOEFN7BbYui2VGzJj7clD5LmarIm",
     GUILD_ID: process.env.GUILD_ID,
     PREFIX: process.env.PREFIX || "-"
 };
